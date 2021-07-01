74bcb5c26c781834760c28f26075ddb5b471ad28
Yohann <yohannassouline@hotmail.fr>
2021-07-01 16:42:25 +0200

    add my fix

--- install.sh
+++ install.sh
@@ -6,21 +6,7 @@ echo -e "\n## Installing prerequisites"
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata
 
-case $esp_idf_version in
-  latest | v4.2 | v4.2.* | release/v4.2)
-    sudo apt-get install -y git wget flex bison gperf python3 python3-pip python3-setuptools python-is-python3 python3-libusb1 cmake ninja-build ccache libffi-dev libssl-dev dfu-util
-    ;;
-  v4.1 | v4.1.* | release/v4.1)
-    sudo apt-get install -y git wget flex bison gperf python3 python3-pip python3-setuptools python-is-python3 python3-libusb1 cmake ninja-build ccache libffi-dev libssl-dev
-    ;;
-  v4.0.* | release/v4.0)
-    sudo apt-get install -y git wget libncurses-dev flex bison gperf python3 python3-pip python3-setuptools python-is-python3 python3-libusb1 cmake ninja-build ccache libffi-dev libssl-dev
-    ;;
-  *)
-    echo "Unimplemented ESP-IDF version !"
-    exit 1
-    ;;
-esac
+sudo apt-get install -y git wget flex bison gperf python3 python3-pip python3-setuptools python-is-python3 python3-libusb1 cmake ninja-build ccache libffi-dev libssl-dev dfu-util
 
 mkdir ~/esp
 cd ~/esp
@@ -38,3 +24,8 @@ cd ~/esp/esp-idf
 
 echo -e "\n## Installing tools"
 ./install.sh > ~/esp-idf-install.log 2>&1
+
+cd ~/esp
+
+# install esp adf
+git clone --recursive $URL_ESP_ADF
